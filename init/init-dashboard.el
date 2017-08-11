5591ea880bc839fff4236d827830d8ed7497f4b8
Samuel Laurén <samuel.lauren@iki.fi>
2017-08-11 14:51:00 +0300

    More

---
+++ init/init-dashboard.el
@@ -0,0 +1,35 @@
+;; -*- mode: Emacs-Lisp; lexical-binding: t; -*-
+;; Dashboard setup
+
+(defvar dashboard-user-banner-directory
+  (f-join user-emacs-directory "banners")
+  "Location for user's dashboard banners.")
+
+(defun dashboard-select-banner ()
+  "Return random file from user's banner directory of 'official."
+  (-if-let (files
+            (and (f-directory? dashboard-user-banner-directory)
+                 (f-files dashboard-user-banner-directory)))
+      (list-random-item files)
+    'official))
+
+(defun dashboard-setup ()
+  "Setup dashboard."
+  (setq-local mode-line-format nil))
+
+(use-package dashboard
+  :ensure t
+  :init
+  (add-hook 'dashboard-mode-hook #'dashboard-setup)
+  :config
+  (setq dashboard-items '((recents . 10)
+                          (projects . 5))
+        dashboard-startup-banner (dashboard-select-banner)
+        dashboard-banner-logo-title
+        (format "Welcome to Emacs, %s %s"
+                (car (s-split " " (user-full-name)))
+                (list-random-item '("🌈" "💖" "🌻" "🌸" "🐬"))))
+  (dashboard-setup-startup-hook))
+
+
+(provide 'init-dashboard)
