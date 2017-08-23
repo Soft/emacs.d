19de165a977614ad1459b1744af456cb89783fd9
Samuel Laurén <samuel.lauren@iki.fi>
2017-08-23 18:58:05 +0300

    More

--- init/init-help.el
+++ init/init-help.el
@@ -18,4 +18,17 @@
 (use-package help-fns+
   :ensure t)
 
+(defconst adequate-url "https://bitbucket.org/Soft/emacs.d/src")
+
+(defun about-adequate-emacs-d ()
+  (interactive)
+  (browse-url adequate-url))
+
+(define-key-after
+  (lookup-key global-map [menu-bar help-menu])
+  [adequate-website]
+  '("About Adequate emacs.d" . about-adequate-emacs-d)
+  'about-gnu-project)
+
 (provide 'init-help)
+
