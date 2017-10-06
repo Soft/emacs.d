723d590f9052aa114385345eefb97d20595fa010
Samuel Laurén <samuel.lauren@iki.fi>
2017-10-06 08:47:39 +0300

    Telephone line tan separator

--- init/init-modeline.el
+++ init/init-modeline.el
@@ -3,6 +3,10 @@
 (use-package telephone-line-config
   :ensure telephone-line
   :config
+  (setq telephone-line-primary-left-separator 'telephone-line-tan-left
+        telephone-line-secondary-left-separator 'telephone-line-tan-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-tan-right
+        telephone-line-secondary-right-separator 'telephone-line-tan-hollow-right)
   (after-load 'evil
     (telephone-line-evil-config)))
 
