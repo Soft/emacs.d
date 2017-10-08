6bc6cbe76031e1e0a1fcbb45fa1e178047483239
Samuel Laurén <samuel.lauren@iki.fi>
2017-10-08 15:38:39 +0300

    Start company mode in after-init-hook

--- init/init-completion.el
+++ init/init-completion.el
@@ -9,7 +9,8 @@
 (use-package company
   :ensure t
   :diminish company-mode
-  :init (global-company-mode)
+  :init
+  (add-hook 'after-init-hook #'global-company-mode)
   :config
   (bind-keys
    :map company-active-map
