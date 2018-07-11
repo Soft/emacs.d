ed367e0190e5c874b0975781dcf27dfca5be323b
Samuel Laurén <samuel.lauren@iki.fi>
2018-07-11 18:27:01 +0300

    Realgud autoloads

--- init/init-debugging.el
+++ init/init-debugging.el
@@ -6,9 +6,21 @@
 
 ;;; Code:
 
+;; TODO: Evil compatibility
 (use-package realgud
   :ensure t
-  :defer t)
+  :commands (realgud:pdb
+             realgud:gdb
+             realgud:jdb
+             realgud:ipdb
+             realgud:perldb
+             realgud:remake
+             realgud:bashdb
+             realgud:node-debug
+             realgud:trepan
+             realgud:trepan2
+             realgud:trepan3k
+             realgud:trepanjs))
 
 (provide 'init-debugging)
 
