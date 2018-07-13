b155791ff3ab3d6fa431c1568ce4502fb11b3b2e
Samuel Laurén <samuel.lauren@iki.fi>
2018-07-13 16:30:50 +0300

    Alternative flycheck segment for modeline

--- init/init-modeline.el
+++ init/init-modeline.el
@@ -8,14 +8,53 @@
 
 (use-package telephone-line-config
   :ensure telephone-line
+  :init
+  ;; Alternative flycheck segment that takes less space, uses all-the-icons and
+  ;; looks better with different themes.
+  (telephone-line-defsegment adq/telephone-line-flycheck-segment ()
+    (when (bound-and-true-p flycheck-mode)
+      (let ((text (pcase flycheck-last-status-change
+                    ('finished (if flycheck-current-errors
+                                   (let-alist (flycheck-count-errors flycheck-current-errors)
+                                     (if (or .error .warning)
+                                         (format "%s %s:%s"
+                                                 (adq/icon-string 'octicon "alert")
+                                                 (or .error 0)
+                                                 (or .warning 0))
+                                       ""))
+                                 (adq/icon-string 'octicon "check")))
+                    ('running (adq/icon-string 'faicon "spinner"))
+                    ('no-checker (adq/icon-string 'octicon "dash"))
+                    ('not-checked (adq/icon-string 'octicon "circle-slash"))
+                    ('errored (adq/icon-string 'octicon "flame"))
+                    ('interrupted (adq/icon-string 'octicon "stop"))
+                    ('suspicious (adq/icon-string 'octicon "question")))))
+        (propertize text
+                    'help-echo (pcase flycheck-last-status-change
+                                 ('finished "Display errors found by Flycheck")
+                                 ('running "Running...")
+                                 ('no-checker "No checker available")
+                                 ('not-checked "Not checked")
+                                 ('errored "Checker error")
+                                 ('interrupted "Checker interrupted")
+                                 ('suspicious "Checker in suspicious state"))
+                    'display '(raise 0.0)
+                    'mouse-face '(:box 1)
+                    'local-map (make-mode-line-mouse-map
+                                'mouse-1 #'flycheck-list-errors)))))
+  (setq telephone-line-rhs
+        '((nil    . (adq/telephone-line-flycheck-segment
+                     telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment))
+          (evil   . (telephone-line-airline-position-segment))))
+
+  (telephone-line-mode)
   :config
   (setq telephone-line-primary-left-separator 'telephone-line-tan-left
         telephone-line-secondary-left-separator 'telephone-line-tan-hollow-left
         telephone-line-primary-right-separator 'telephone-line-tan-right
         telephone-line-secondary-right-separator 'telephone-line-tan-hollow-right
-        telephone-line-evil-use-short-tag t)
-  (adq/after-load 'evil
-    (telephone-line-evil-config)))
+        telephone-line-evil-use-short-tag t))
 
 (provide 'init-modeline)
 
