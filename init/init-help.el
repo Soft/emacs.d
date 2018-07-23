324dec2de9fce7d5bf13b3e3c8c4398cdb6759fe
Samuel Laurén <samuel.lauren@iki.fi>
2018-07-23 10:34:16 +0300

    Add key based replacements to which-key

--- init/init-help.el
+++ init/init-help.el
@@ -21,8 +21,15 @@
   :ensure t
   :diminish which-key-mode
   :init
+  (which-key-mode)
+  :config
+  (defvar which-key-idle-delay)
   (setq which-key-idle-delay 0.5)
-  (which-key-mode))
+  (which-key-add-key-based-replacements
+    "C-c d" "tags"
+    "C-c x" "misc"
+    "C-c o" "org"
+    "C-c p" "project"))
 
 (defvar adq/helpful-reuse-buffers t
   "Should helpful reuse existing buffers.")
