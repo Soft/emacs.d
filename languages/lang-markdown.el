4365ec9fe9e12997c3da5686a3d8424f4333041e
Samuel Laurén <samuel.lauren@iki.fi>
2018-09-01 19:12:28 +0300

    Flycheck checker for finding internal links without targets

--- languages/lang-markdown.el
+++ languages/lang-markdown.el
@@ -51,6 +51,40 @@
 
 ;; Patch markdown-mode link jumping to work with links internal to document
 
+(defun adq/markdown-derive-auto-identifier (title)
+  "Derive identifier based on title. The steps to do this are
+  outlined in the Pandoc manual. This is not a complete
+  implementation as it does not remove all formating from the
+  title as this would require a proper parser."
+  (let ((identifier
+         (thread-last title
+           (downcase)
+           (replace-regexp-in-string " " "-")
+           (replace-regexp-in-string (rx line-start (0+ (not alpha))) ""))))
+    (if (string= "" identifier)
+        "section"
+      identifier)))
+
+(defun adq/markdown-list-titles ()
+  "Return list of (SECTION-TITLE . POSITION) pairs."
+  (save-match-data
+    (save-excursion
+      (goto-char (point-min))
+      (cl-loop while (re-search-forward
+                      (rx line-start
+                          (repeat 1 6 "#")
+                          (0+ whitespace)
+                          (group (1+ not-newline)))
+                      nil t)
+               collect (cons (match-string-no-properties 1) (match-beginning 1))))))
+
+(defun adq/markdown-title-identifiers ()
+  "Return hash table with document identifers as keys."
+  (let ((hash (make-hash-table :test 'equal)))
+    (dolist (title (adq/markdown-list-titles))
+      (puthash (adq/markdown-derive-auto-identifier (car title)) t hash))
+    hash))
+
 (defun adq/markdown-jump-to-top-level-header (title)
   (if-let ((location
             (save-excursion
@@ -69,6 +103,43 @@
               (message "%s" link))
           (call-interactively fn)))
     (error "No link at point")))
+
+;; Flycheck checker for invalid internal links
+
+(defun adq/markdown-check-internal-links (checker callback)
+  "Checker function for Flycheck. The checker checks the buffer
+  for internal links that do not have a target."
+  (let ((identifiers (adq/markdown-title-identifiers))
+        (errors '()))
+    (dolist (link-type (list markdown-regex-link-inline
+                             markdown-regex-link-reference
+                             markdown-regex-uri
+                             markdown-regex-angle-uri))
+      (save-match-data
+        (save-excursion
+          (goto-char (point-min))
+          (cl-loop while (re-search-forward link-type nil t)
+                   for url = (markdown-link-url)
+                   when (string-prefix-p "#" url)
+                   unless (gethash (adq/markdown-derive-auto-identifier (substring url 1))
+                                   identifiers nil)
+                   do (push (flycheck-error-new-at (line-number-at-pos) (match-beginning 0)
+                                                   'error
+                                                   (format "Link target %s does not exist" url)
+                                                   :checker checker)
+                            errors)))))
+    (funcall callback 'finished errors)))
+
+(flycheck-define-generic-checker 'adq/markdown-internal-links
+  "Checker for finding internal links with missing targets."
+  :start #'adq/markdown-check-internal-links
+  :modes '(markdown-mode))
+
+(defun adq/markdown-setup-internal-link-checker ()
+  "Enable Flycheck checker for finding internal links with
+missing targets."
+  (interactive)
+  (add-to-list 'flycheck-checkers 'adq/markdown-internal-links t))
 
 (defun adq/markdown-setup ()
   (when (locate-library "pandoc-mode")
