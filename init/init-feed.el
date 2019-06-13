238c21d7e89b35bb1bb2c7201ff39b64f608ef2f
Samuel Laurén <samuel.lauren@iki.fi>
2019-06-13 13:37:26 +0300

    Specify maximum width for elfeed-show-mode

--- init/init-feed.el
+++ init/init-feed.el
@@ -2,13 +2,30 @@
 
 ;;; Commentary:
 
-;; Feed aggregator configuration
+;; Feed aggregator configuration.
+
+;; Set elfeed-feeds to a list of URLs for feeds.
 
 ;;; Code:
 
+
+(defvar adq/elfeed-display-width 80
+  "Content width for elfeed-show-mode.")
+
+
+(defun adq/elfeed-show-setup ()
+  "Setup elfeed-show-mode."
+  (when (-all?
+         (lambda (window)
+           (> (window-body-width window)
+              adq/elfeed-display-width))
+         (get-buffer-window-list))
+    (setq-local shr-width adq/elfeed-display-width))
+  (setq-local shr-bullet "• "))
+
+
 (use-package elfeed
   :ensure t
-  :defer t
   :bind
   (("C-c x F" . elfeed)
    :map elfeed-search-mode-map
@@ -16,13 +33,18 @@
    ("k" . previous-line)
    ("K" . scroll-down)
    ("J" . scroll-up)
+   ("o" . elfeed-show-entry)
+   ("/" . elfeed-search-set-filter)
    :map elfeed-show-mode-map
+   ("f" . eww-follow-link)
    ("j" . next-line)
    ("k" . previous-line)
    ("h" . left-char)
    ("l" . right-char)
    ("K" . scroll-down)
-   ("J" . scroll-up)))
+   ("J" . scroll-up))
+  :config
+  (add-hook 'elfeed-show-mode-hook #'adq/elfeed-show-setup))
 
 (provide 'init-feed)
 
