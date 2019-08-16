3f1195b7b54341835dd939ee7720763e64f9779c
Samuel Laurén <samuel.lauren@iki.fi>
2019-08-16 16:23:54 +0300

    Add company-go, go-eldoc and go-playground

--- languages/lang-go.el
+++ languages/lang-go.el
@@ -22,7 +22,28 @@
   (setq-local prettify-symbols-alist adq/go-prettify-symbols-alist)
   (add-hook 'before-save-hook #'gofmt-before-save nil t)
   (adq/setq-local compilation-read-command nil
-                  compile-command "go build"))
+                  compile-command "go build")
+  (when (package-installed-p 'go-eldoc)
+    (go-eldoc-setup))
+  (when (package-installed-p 'company-go)
+    (setq-local company-backends '(company-go))))
+
+(use-package company-go
+  :if (adq/programs-p "gocode")
+  :ensure t
+  :defer t)
+
+(use-package go-eldoc
+  :if (adq/programs-p "gocode")
+  :ensure t
+  :defer t)
+
+(use-package go-playground
+  :ensure t
+  :defer t
+  :bind
+  (:map go-playground-mode
+        ("C-c C-c" . go-playground-exec)))
 
 (use-package go-mode
   :mode (("\\.go\\'" . go-mode))
