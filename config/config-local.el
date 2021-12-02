2e984c2ee9d58246e2bd619c72b2ba92ce501ddd
Samuel Laurén <samuel.lauren@iki.fi>
2021-12-02 17:04:20 +0200

    Refactor adq/load-local-files to use thread-last

--- config/config-local.el
+++ config/config-local.el
@@ -8,18 +8,18 @@
   "Directory from where `adq/load-local-files' will load additional configuration.")
 
 (defun adq/load-local-files (&optional force)
-  (mapcar
-   (lambda (path)
-     (when adq/emacs-debug
-       (message "Loading %s" path))
-     (puthash path t adq/loaded-local-files)
-     (load path nil nil t)
-     path)
-   (seq-sort-by #'file-name-base #'string<
-                (seq-filter (lambda (path)
-                              (or force
-                                  (not (gethash path adq/loaded-local-files))))
-                            (directory-files adq/local-files-directory t "\\.el\\'")))))
+  (thread-last (directory-files adq/local-files-directory t "\\.el\\'")
+    (seq-filter (lambda (path)
+                  (or force
+                      (not (gethash path adq/loaded-local-files)))))
+    (seq-sort-by #'file-name-base #'string<)
+    (mapcar
+     (lambda (path)
+       (when adq/emacs-debug
+         (message "Loading %s" path))
+       (puthash path t adq/loaded-local-files)
+       (load path nil nil t)
+       path))))
 
 (add-hook 'after-init-hook #'adq/load-local-files)
 
