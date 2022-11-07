2902f59c8640dcbb5ceb19fd8d298558ea352e95
Samuel Laurén <samuel.lauren@iki.fi>
2022-11-07 12:43:36 +0200

    Add evil-textobj-tree-sitter

--- config/config-vim.el
+++ config/config-vim.el
@@ -41,6 +41,55 @@
   (setq evil-collection-key-blacklist '("J" "K"))
   (evil-collection-init))
 
+(use-package evil-textobj-tree-sitter
+  :after (tree-sitter evil)
+  :config
+  (bind-key "f"
+            (evil-textobj-tree-sitter-get-textobj "function.outer")
+            evil-outer-text-objects-map)
+  (bind-key "f"
+            (evil-textobj-tree-sitter-get-textobj "function.inner")
+            evil-inner-text-objects-map)
+  (bind-key "b"
+            (evil-textobj-tree-sitter-get-textobj "block.outer")
+            evil-outer-text-objects-map)
+  (bind-key "b"
+            (evil-textobj-tree-sitter-get-textobj "block.inner")
+            evil-inner-text-objects-map)
+  (bind-key "c"
+            (evil-textobj-tree-sitter-get-textobj "conditional.outer")
+            evil-outer-text-objects-map)
+  (bind-key "c"
+            (evil-textobj-tree-sitter-get-textobj "conditional.inner")
+            evil-inner-text-objects-map)
+  (bind-key "l"
+            (evil-textobj-tree-sitter-get-textobj "loop.outer")
+            evil-outer-text-objects-map)
+  (bind-key "l"
+            (evil-textobj-tree-sitter-get-textobj "loop.inner")
+            evil-inner-text-objects-map)
+  (bind-key "a"
+            (evil-textobj-tree-sitter-get-textobj "call.outer")
+            evil-outer-text-objects-map)
+  (bind-key "a"
+            (evil-textobj-tree-sitter-get-textobj "call.inner")
+            evil-inner-text-objects-map)
+  (bind-key "P"
+            (evil-textobj-tree-sitter-get-textobj "parameter.outer")
+            evil-outer-text-objects-map)
+  (bind-key "P"
+            (evil-textobj-tree-sitter-get-textobj "parameter.inner")
+            evil-inner-text-objects-map)
+  (bind-key "C"
+            (evil-textobj-tree-sitter-get-textobj "class.outer")
+            evil-outer-text-objects-map)
+  (bind-key "C"
+            (evil-textobj-tree-sitter-get-textobj "class.inner")
+            evil-inner-text-objects-map)
+  (bind-key "s"
+            (evil-textobj-tree-sitter-get-textobj "statement.outer")
+            evil-outer-text-objects-map))
+
 (use-package evil-surround
   :after evil
   :config
